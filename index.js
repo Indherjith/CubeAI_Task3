38231afbf0a5770758dc7cc267c6791e9d143b98
Indherjith <buvanesifet26@gmail.com>
2024-04-12 07:52:04 +0530

    main

---
+++ index.js
@@ -0,0 +1,227 @@
+var fulname = false;
+var usename = false;
+var pasword = false;
+var conpasword = false;
+var mai = false;
+var cont = false;
+var gen = false;
+
+document.getElementById("loader").style.display = "none";
+
+document.querySelector('form').addEventListener('submit',async (e) => {
+    e.preventDefault()
+    document.getElementsByTagName("body")[0].style.opacity = 0.4;
+    const data = Object.fromEntries(new FormData(e.target).entries())
+    document.getElementById("loader").style.display = "block";
+    try{
+        const response = await fetch("http://localhost:8080/register", {method: "POST",mode: "cors",
+        headers: {"Content-Type": "application/json"},body:JSON.stringify(data)})
+        const res = await response.json();
+        document.getElementsByTagName("body")[0].style.opacity = 1;
+        alert(res.msg)
+        document.getElementById("loader").style.display = "none";
+    }
+    catch(err){
+        document.getElementsByTagName("body")[0].style.opacity = 1;
+        console.log(err);
+    }
+    
+
+});
+
+const check_Fullname = (fname)=>{ 
+    var name = fname.split("");
+    var status;    
+    name.map(item=>{
+        if(Number(item)){
+            status= false;
+        }
+        else{
+            const specialChars = "/[`!@#$%^&*()_+-=[]{};':\|,.<>/?~]/".split("");
+            const specialchar = specialChars.map(elem=>{
+                if(elem == item){
+                    status = false;
+                }
+            })
+        }
+    })
+    if(fname === null || fname === ""){
+        let stat = document.getElementById("full_msg");
+        stat.innerHTML ="* Invalid Name!, a-z & A-Z are Acceptable.";
+        stat.style.color = "orangered";
+        fulname = false;
+        activebtn();
+        document.getElementById("fname").value = null;
+        return;
+    }
+    if(status == false){
+        let stat = document.getElementById("full_msg");
+        stat.innerHTML ="* Invalid Name!, a-z & A-Z are Acceptable.";
+        stat.style.color = "orangered";
+        fulname = false;
+        activebtn();
+        document.getElementById("fname").value = null;
+        return;
+    }
+    else{
+        let stat = document.getElementById("full_msg");
+        stat.innerHTML ="* Validation Success 🗸🗸";
+        stat.style.color = "lightgreen";
+        fulname = true;
+        activebtn();
+        return;
+    }
+}
+
+const check_Username = (uname)=>{
+    var name = uname.split("");
+    var status;    
+    name.map(item=>{
+        const specialChars = "/[`!#$%^&*()_+-=[]{};':\|,.<>/?~]/".split("");
+        const specialchar = specialChars.map(elem=>{
+            if(elem == item){
+                status = false;
+            }
+        })
+    })
+    if(status == false){
+        let stat = document.getElementById("user_msg");
+        stat.innerHTML ="* Invalid UserName!, Aa-Zz & 1-10 & @ are Acceptable.";
+        stat.style.color = "orangered";
+        usename = false;
+        activebtn();
+        document.getElementById("uname").value = null;
+    }
+    else{
+        let stat = document.getElementById("user_msg");
+        stat.innerHTML ="* Validation Success 🗸🗸";
+        stat.style.color = "lightgreen";
+        usename = true;
+        activebtn();
+    }
+}
+
+const check_password = (pass)=>{
+    var password = pass.split("");
+    var num=0;
+    var special = 0;
+    if(pass.length >= 8){
+        password.map(item=>{
+            if(Number(item)){
+                num+=1;
+            }
+            else{
+                const specialChars = "/[`!@#$%^&*()_+-=[]{};':\|,.<>/?~]/".split("");
+                const specialchar = specialChars.map(elem=>{
+                    if(elem == item){
+                        special+=1;
+                    }
+                })
+            }
+        })
+        if(num<=0 || special<=0){
+            let stat = document.getElementById("pass_msg");
+            stat.innerHTML ="* Atleast 1-num & 1-spec.Char requiorangered!";
+            stat.style.color = "orangered";
+            pasword = false;
+            activebtn();
+            document.getElementById("pass").value = null;
+        }
+        else{
+            let stat = document.getElementById("pass_msg");
+            stat.innerHTML ="* Validation Success 🗸🗸";
+            stat.style.color = "lightgreen";
+            pasword = true;
+            activebtn();
+        }
+    }
+    else{
+        let stat = document.getElementById("pass_msg");
+            stat.innerHTML ="* Weak password! Min (8 char) requiorangered!";
+            stat.style.color = "orangered";
+            document.getElementById("pass").value = null;
+            pasword = false;
+            activebtn();
+    }
+}
+
+const check_confirmpassword=(pass)=>{
+    let password = document.getElementById("pass").value;
+    if(pass !== password){
+        let stat = document.getElementById("cpass_msg");
+        stat.innerHTML ="* Confirm Password & Password must be same!";
+        stat.style.color = "orangered";
+        conpasword = false;
+        activebtn();
+        document.getElementById("conpass").value = null;
+    }
+    else{
+        let stat = document.getElementById("cpass_msg");
+        stat.innerHTML ="* Validation Success 🗸🗸";
+        stat.style.color = "lightgreen";
+        conpasword = true;
+        activebtn();
+    }
+}
+
+function check_mail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    let stat = document.getElementById("mail_msg");
+    stat.innerHTML ="* Validation Success 🗸🗸";
+    stat.style.color = "lightgreen";
+    mai = true;
+    activebtn();
+    return;
+  }
+    let stat = document.getElementById("mail_msg");
+    stat.innerHTML ="* Invalid email address!";
+    stat.style.color = "orangered";
+    mai = false;
+    activebtn();
+    document.getElementById("mail").value = null;
+}
+
+function check_contact(num){
+    const status = /^\d{10}$/.test(num);
+    if(status){
+        let stat = document.getElementById("contact_msg");
+        stat.innerHTML ="* Validation Success 🗸🗸";
+        stat.style.color = "lightgreen";
+        cont = true;
+        activebtn();
+        return;
+    }
+    else{
+        let stat = document.getElementById("contact_msg");
+        stat.innerHTML ="* Invalid Contact Number!";
+        stat.style.color = "orangered";
+        cont = false;
+        activebtn();
+        document.getElementById("contact").value = null;
+    }
+}
+
+function check_gender(e){
+    gen = true;
+    let stat = document.getElementById("gender_msg");
+    stat.innerHTML ="* Validation Success 🗸🗸";
+    stat.style.color = "lightgreen";
+    activebtn();
+    return;
+}
+
+
+function activebtn(){
+    if(((fulname && usename)&&(pasword && conpasword))&&(mai && gen) ){
+        document.getElementById("sub_btn").disabled = false;
+        document.getElementById("sub_btn").style.opacity = 1;
+    }
+    else{
+        document.getElementById("sub_btn").disabled = true;
+        document.getElementById("sub_btn").style.opacity = 0.5;
+    }
+}
+
+activebtn();
